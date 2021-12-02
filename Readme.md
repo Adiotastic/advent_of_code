25d543b2e1ad36d6989d5dbb3471590e49aebd7d
Christian Heinz <christian.heinz@mhp.com>
2021-12-02 14:47:13 +0100

    Initial checkin with day1 and day2

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Advent of Code Repo
+
+Advent calendar with tasks found under [adventofcode.com](https://adventofcode.com/2021)
+
+## Install
+
+Run `npm i` from root.
+
+## Execute
+
+From root execute `ts-node ./lib/index.ts` and get result from terminal.
---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+import { Day1 } from './days/day1';
+import { Day2 } from './days/day2';
+
+const result1_1: Number = Day1.part1();
+console.log('# Result: ' + result1_1);
+
+const result1_2: Number = Day1.part2();
+console.log('# Result: ' + result1_2);
+
+const result2_1: Number = Day2.part1();
+console.log('# Result: ' + result2_1);
+
+const result2_2: Number = Day2.part2();
+console.log('# Result: ' + result2_2);
